ecc693fab861864bc55650b2035b7415f0ebfed4
u586122 <Gryan@personal.com.ar>
2018-12-20 17:12:18 -0300

    no message

--- app/routes/association.routes.js
+++ app/routes/association.routes.js
@@ -1,18 +1,34 @@
 module.exports = (app) => {
+	
+	
     const association = require('../controllers/association.controller.js');
+	const user=process.env.ASSOCIATION_USER;
+	const pass=process.env.ASSOCIATION_PASSWORD;
+	
+	var staticUserAuth = basicAuth({
+		users: {
+			user : pass
+		},
+		challenge: false,
+		unauthorizedResponse: getUnauthorizedResponse
+	});
 
     // Create a new Association
-    app.post('/association', association.create);
+    app.post('/association',staticUserAuth, association.create);
 
     // Retrieve all Associations
-    app.get('/association', association.findAll);
+    app.get('/association',staticUserAuth, association.findAll);
 
     // Retrieve a single Association with noteId
-    app.get('/association/:id_consumer', association.findOne);
+    app.get('/association/:id_consumer',staticUserAuth, association.findOne);
 
     // Update a Association with noteId
-    app.put('/association/:id_consumer', association.update);
+    app.put('/association/:id_consumer',staticUserAuth, association.update);
 
     // Delete a Association with noteId
-    app.delete('/association/:id_consumer', association.delete);
+    app.delete('/association/:id_consumer',staticUserAuth, association.delete);
+	
+	function getUnauthorizedResponse(req) {
+		return req.auth ? {"message":"Credentials with user" + req.auth.user + " rejected"} : {"message" : "No credentials provided"}
+}
 }
